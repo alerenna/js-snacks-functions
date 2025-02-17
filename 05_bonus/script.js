91862b8a908f8ff2985876404276ba8d081707bc
“alerenna” <alessandrorennafm@gmail.com>
2025-02-17 21:29:50 +0800

    Add solution snack 5

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,9 +9,30 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function greetingsDaytimeName(string) {
+    //Inizializzo la variabile per il conteggio dell'orario
+    const d = new Date();
+    let dayTime = d.getHours()
 
+    //Inizializzo la variabile per la stringa da restituire
+    let greetings = ''
+    
+    //SE l'ora è fino alle 13 stampo Buongiorno + stringa | SE da 13 a 17 stampo buon pomeriggio | Se oltre 17 stampo buonasera
+
+    if (dayTime < 13) {
+        console.log(`Buongiono ${string}`);
+    } else if(dayTime > 13 && dayTime < 17) {
+        console.log(`Buon pomeriggio ${string}`);
+    } else {
+        console.log(`Buonasera ${string}`);
+    }
+
+    return greetings
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const greetingsDayTime = greetingsDaytimeName('Mario')
+console.log(greetingsDayTime);
 
 
 
