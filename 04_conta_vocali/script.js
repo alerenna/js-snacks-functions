d6b4c845be72f2c6fb891235383e9676937e3801
“alerenna” <alessandrorennafm@gmail.com>
2025-02-17 20:42:17 +0800

    Add solution snack 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,25 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(string) {
+    //Inizializzo la variabile che conta le vocali nella stringa
+    let vowelsNumber = 0
 
+    //Controllo tutte le lettere della stringa
+    for (let i = 0; i < string.length; i++) {
+        const thisLetter = string[i]
+
+        //SE la lettera è una delle vocali allora conto +1 nella variabile delle vocali
+        if (thisLetter === 'a' || thisLetter === 'e' || thisLetter === 'i' || thisLetter === 'o' || thisLetter === 'u') {
+            vowelsNumber += 1
+        }
+    }
+    return vowelsNumber
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const vowels = countVowels('javascript')
+console.log(vowels);
 
 
 
