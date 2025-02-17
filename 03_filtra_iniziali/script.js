3eef8f26d53d563af732730248785abd2d18c809
“alerenna” <alessandrorennafm@gmail.com>
2025-02-17 20:33:26 +0800

    Add solution snack 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function wordStartWithLetter(arr,letter) {
+    //Dichiaro array che contiene le parole che iniziano con la lettera indicata
+    const wordInitial = []
 
+    //Cerco nell'array tutte le parole
+    for (let i = 0; i < arr.length; i++) {
+        const thisWord = arr[i]
+
+        //SE la parola trovata inizia con la lettera indicata allora la inserisco nella nuova lista
+        if (thisWord.startsWith(letter)){
+            wordInitial.push(thisWord)
+        }
+
+    }
+
+    return wordInitial
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+const wordWithLetter = wordStartWithLetter(["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"], 'A')
+console.log(wordWithLetter);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
